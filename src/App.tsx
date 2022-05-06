fa64824c058705397458f38c3b19ff9607edbcad
Bazee-code <eugeneobazee2@gmail.com>
2022-05-06 07:20:58 +0300

    Render all items component that displays all added and completed tasks views

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import AddItemsInput from './components/AddItemsInput';
+import AllItems from './components/AllItems';
 import Header from './components/Header';
 
 const AppContainer = styled.div`
@@ -11,6 +12,7 @@ const App = () => {
     <AppContainer>
       <Header />
       <AddItemsInput />
+      <AllItems />
     </AppContainer>
   );
 };
