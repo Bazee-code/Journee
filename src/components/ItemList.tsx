5f94690e67377a40e697e1d6a39d00d69d74a688
Bazee-code <eugeneobazee2@gmail.com>
2022-05-06 09:50:26 +0300

    Access redux store, fetch created tasks & display them

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,54 +1,76 @@
 import React from "react";
 import styled from "styled-components";
-import { addTodo, editTodo } from "../todo/todoAC";
+import { editTodo } from "../todo/todoAC";
 import { useAppDispatch, useAppSelector } from "../types";
 
+const ItemListContainer = styled.div`
+  width : 30%;
+  background-color : yellow;
+  border-radius : 3%;
+`
+const Title = styled.p`
+    color : #000;
+    text-align : center;
+    font-size : 18px;
+    font-weight : 800;
+`
 const List = styled.ul`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  box-sizing: border-box;
+  // width: 100%;
+  // display: flex;
+  // flex-direction: column;
+  // box-sizing: border-box;
 `;
 
 const Item = styled.li`
-  width: 100%;
-  margin: 4px 0;
-  display: flex;
-  flex-direction: row;
+  // width: 100%;
+  // margin: 4px 0;
+  // display: flex;
+  // flex-direction: row;
 `;
 
 const ItemInput = styled.input`
-  width: 100%;
+  height :28px;
+  width : 70%;
 `;
 
 const ItemCheckbox = styled.input.attrs({
   type: "checkbox",
 })``;
 
+const ItemListText = styled.p`
+    text-align : center;
+    font-size : 16px;
+    color : grey;
+`
+
 const ItemList: React.FC = () => {
   const items = useAppSelector((s) => s.todos.items);
   const dispatch = useAppDispatch();
+
   return (
+    <ItemListContainer>
+    <Title>Your Tasks</Title>
     <List>
-      {items.map((item) => (
-        <Item key={item.id}>
-          <ItemCheckbox />
-          <ItemInput
-            type="text"
-            value={item.text}
-            onChange={(e) => dispatch(editTodo(item.id, e.target.value))}
-          />
-        </Item>
-      ))}
-      <Item key="new">
-        <ItemCheckbox />
-        <ItemInput
-          type="text"
-          value=""
-          onChange={(e) => dispatch(addTodo(e.target.value))}
-        />
-      </Item>
+      {
+        items.length === 0 ? <ItemListText>You currently have no tasks</ItemListText> : (
+          items.map((item) => {
+            if (item.done === false){
+              return (
+                <Item key={item.id}>
+                <ItemCheckbox  />
+                <ItemInput
+                type="text"
+                value={item.text}
+                onChange={(e) => dispatch(editTodo(item.id, e.target.value))}
+              />
+            </Item>
+              )
+            }
+          })
+        )
+      }
     </List>
+    </ItemListContainer>
   );
 };
 
