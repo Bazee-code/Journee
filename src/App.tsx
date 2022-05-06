303254abadc1075f21f2373252d43966e725a57b
Bazee-code <eugeneobazee2@gmail.com>
2022-05-06 06:56:02 +0300

    Create and render app header component, style parent app container

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import styled from 'styled-components'
+import Header from './components/Header';
 import ItemList from "./components/ItemList";
+
+const AppContainer = styled.div`
+  padding : 30px;
+`
 
 const App = () => {
   return (
-    <div>
-      <h2>Todo</h2>
+    <AppContainer>
+      <Header />
       <ItemList />
-    </div>
+    </AppContainer>
   );
 };
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const H2 = styled.h2`
+    text-align : center
+`
+
+const Header = () => {
+    
+    return (
+        <div>
+            <H2>Todo List App</H2>
+        </div>
+    )
+}
+
+export default Header;
