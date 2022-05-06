593fc66e632d0e9c2dadefef186eb68b176fdc7a
Bazee-code <eugeneobazee2@gmail.com>
2022-05-06 15:43:44 +0300

    Migrate add item input styles

---
+++ src/styles/input-styles.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const InputContainer = styled.div`
+    width : 100%;
+    display : flex;
+    justify-content : center;
+`
+
+export const InputElement = styled.input`
+    width : 30%;
+    height : 30px;
+    cursor : pointer;
+`
+export const Button = styled.button`
+    margin-left : 5px;
+    width : 5%;
+    cursor : pointer;
+    background-color : green;
+    color : #FFF;
+    border : none;
+`
